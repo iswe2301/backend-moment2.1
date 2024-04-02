24468070685ca16e52c3898c22bc0b261bc6946c
Isa Westling <isawestling.blogg@hotmail.com>
2024-04-02 15:03:24 +0200

    Skapat route för POST inkl. felhantering.

--- server.js
+++ server.js
@@ -39,6 +39,8 @@ client.connect((error) => {
 });
 
 // Skapar routes
+
+// Route för GET
 app.get("/api/workexperience", (req, res) => {
     // Hämtar alla alla jobberfarenheter från DB
     client.query(`SELECT * FROM workexperience;`, (err, results) => {
@@ -56,6 +58,70 @@ app.get("/api/workexperience", (req, res) => {
             // Annars skickas resultatet
             res.json(results.rows);
         }
+    });
+});
+
+// Route för POST
+app.post("/api/workexperience", (req, res) => {
+    let companyname = req.body.companyname;
+    let jobtitle = req.body.jobtitle;
+    let location = req.body.location;
+    let startdate = req.body.startdate;
+    let enddate = req.body.enddate;
+    let description = req.body.description;
+
+    // Kontrollerar om slutdatum är tom textsträng, sätter till NULL
+    if (enddate === "") {
+        enddate = null;
+    }
+
+    // Skapar felhantering
+    let errors = {
+        message: "",
+        details: "",
+        https_response: {
+
+        }
+    };
+
+    // Kontrollerar om företagsnamn, jobbtitel, plats startdatum eller beskrivning saknas
+    if (!companyname || !jobtitle || !location || !startdate || !description) {
+        // Skriver ut error-meddelanden isåfall
+        errors.message = "Företagsnamn, jobbtitel, plats, startdatum och beskrivning ej inkluderat";
+        errors.details = "Du måste inkludera företagsnamn, jobbtitel, plats, startdatum samt beskrivning JSON";
+
+        // Skriver ut error-felkod
+        errors.https_response.message = "Bad request";
+        errors.https_response.code = 400;
+        res.status(400).json(errors);
+        return; // Avbryter funktionen
+    }
+
+    // Lägger till nya jobberfarenheter till databasen
+    client.query(`INSERT INTO workexperience(companyname, jobtitle, location, startdate, enddate, description) VALUES($1, $2, $3, $4, $5, $6);`, [companyname, jobtitle, location, startdate, enddate, description], (err, results) => {
+        // Kontrollerar om fel finns
+        if (err) {
+            // Skriver ut error med felkod
+            res.status(500).json({ error: "Något gick fel: " + err });
+            return; // Avrbyter
+        }
+
+        // Loggar meddelande om fråga har lyckats skapas
+        console.log("Fråga skapad!");
+
+        // Skapar ett objekt som representerar jobberfarenheten
+        let workexperience = {
+            companyname: companyname,
+            jobtitle: jobtitle,
+            location: location,
+            startdate: startdate,
+            enddate: enddate,
+            description: description
+        }
+
+        // Skickar ett svar med meddelande om lyckad tilläggning och detaljerna om den tillagda jobberfarenheten
+        res.json({ message: "Jobberfarenhet tillagd", workexperience });
+
     });
 });
 
