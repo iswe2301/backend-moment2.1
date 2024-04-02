4bc0849ef74039d6bd00a1c10c1883f651ec61f6
Isa Westling <isawestling.blogg@hotmail.com>
2024-04-02 15:59:09 +0200

    Skapat route för PUT (update) + felhantering.

--- server.js
+++ server.js
@@ -63,6 +63,7 @@ app.get("/api/workexperience", (req, res) => {
 
 // Route för POST
 app.post("/api/workexperience", (req, res) => {
+    // Läser in den skickade datan
     let companyname = req.body.companyname;
     let jobtitle = req.body.jobtitle;
     let location = req.body.location;
@@ -123,6 +124,79 @@ app.post("/api/workexperience", (req, res) => {
         res.json({ message: "Jobberfarenhet tillagd", workexperience });
 
     });
+});
+
+// Route för PUT (update), skickar med id för specifik tabellrad som ska uppdateras
+app.put("/api/workexperience/:id", (req, res) => {
+    // Läser in den skickade datan
+    const id = req.params.id;
+    let companyname = req.body.companyname;
+    let jobtitle = req.body.jobtitle;
+    let location = req.body.location;
+    let startdate = req.body.startdate;
+    let enddate = req.body.enddate;
+    let description = req.body.description;
+
+    // Kontrollerar om slutdatum är tom textsträng, sätter till NULL
+    if (enddate === "") {
+        enddate = null;
+    }
+
+    // Skapar felhantering
+    let errors = {
+        message: "",
+        details: "",
+        https_response: {
+
+        }
+    };
+
+    // Kontrollerar om företagsnamn, jobbtitel, plats startdatum eller beskrivning saknas
+    if (!companyname || !jobtitle || !location || !startdate || !description) {
+        // Skriver ut error-meddelanden isåfall
+        errors.message = "Företagsnamn, jobbtitel, plats, startdatum och beskrivning ej inkluderat";
+        errors.details = "Du måste inkludera företagsnamn, jobbtitel, plats, startdatum samt beskrivning JSON";
+
+        // Skriver ut error-felkod
+        errors.https_response.message = "Bad request";
+        errors.https_response.code = 400;
+        res.status(400).json(errors);
+        return; // Avbryter funktionen
+    }
+
+    // Uppdaterar tabellen workexperience på specifik rad som stämmer överrens med ID, sätter in de nya värdena
+    client.query(
+        `UPDATE workexperience SET companyname=$1, jobtitle=$2, location=$3, startdate=$4, enddate=$5, description=$6 WHERE id=$7;`, [companyname, jobtitle, location, startdate, enddate, description, id], (err, result) => {
+            // Kontrollerar om fel finns
+            if (err) {
+                // Skriver ut felmeddelande med felkod isåfall och avbryter funktionen
+                res.status(500).json({ error: "Något gick fel vid uppdatering: " + err });
+                return;
+            }
+            // Kontrollerar om antalet påverkade rader är 0 (inget resultat stämmer med id)
+            if (result.rowCount === 0) {
+                // Skriver ut felmeddelande isåfall och avbryter
+                res.status(404).json({ message: "Jobberfarenhet inte funnen." });
+                return;
+            } else {
+                // Loggar meddelande om fråga har uppdaterats
+                console.log("Fråga uppdaterad!");
+
+                // Skapar ett objekt som representerar jobberfarenheten
+                let workexperience = {
+                    companyname: companyname,
+                    jobtitle: jobtitle,
+                    location: location,
+                    startdate: startdate,
+                    enddate: enddate,
+                    description: description
+                }
+
+                // Skickar ett svar med meddelande om lyckad tilläggning och detaljerna om den tillagda jobberfarenheten
+                res.json({ message: "Jobberfarenhet uppdaterad", workexperience });
+            }
+        }
+    );
 });
 
 // Startar applikationen/servern
